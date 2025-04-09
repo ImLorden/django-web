258aec49ac477b14ad7fa0046d82180a3dc7baaf
JayLorden <l12345rj@gmail.com>
2025-04-10 07:17:47 +0800

    5

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.db import OperationalError
 from .models import Post
 
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/home.html', {'posts': posts})
+    try:
+        posts = Post.objects.filter(is_published=True).order_by('-created_at')
+        return render(request, 'blog/home.html', {'posts': posts})
+    except OperationalError:
+        # Handle the case where the table doesn't exist yet
+        return render(request, 'blog/home.html', {'posts': [], 'setup_required': True})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
